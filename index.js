afd5089d180eff54457c70ea10473941546601e8
omesh139 <119126407+omesh139@users.noreply.github.com>
2025-10-10 13:07:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const express = require("express");
-const app = express();
-__path = process.cwd();
 const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 8000;
-let code = require("./pair");
+const path = require("path");
+
 require("events").EventEmitter.defaultMaxListeners = 500;
-app.use("/code", code);
 
-app.use("/", async (req, res, next) => {
-  res.sendFile(__path + "/pair.html");
-});
+const app = express();
+const code = require("./pair");
 
+// Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(PORT, () => {
-  console.log(`⏩ Server running on http://localhost:` + PORT);
+
+// Routes
+app.use("/code", code);
+
+// Serve HTML file
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/pair.html"));
 });
 
+// Export app for Vercel
 module.exports = app;
